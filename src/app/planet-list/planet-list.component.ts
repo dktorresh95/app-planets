759fc44ddfca74f5a3ec1562ec8ad2b96b72fd66
dktorres1995 <dktorres95@gmail.com>
2025-02-01 01:23:08 -0500

    Se crean componentes, se instalan dependencias

---
+++ src/app/planet-list/planet-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-list',
+  imports: [],
+  templateUrl: './planet-list.component.html',
+  styleUrl: './planet-list.component.css'
+})
+export class PlanetListComponent {
+
+}
